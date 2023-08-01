775ed4e5292a237ecabf0663617fcbb4d874bd47
Quincy Maynor <quincymaynor@gmail.com>
2023-07-31 23:47:28 -0500

    fixed all the bonus delete functions

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const promptUser = () => {
       type: 'list',
       name: 'action',
       message: 'What would you like to do?',
-      choices:['View all departments', 'Add a department', 'Delete a department', 'View all roles', 'Add a role', 'View all employees', 'Add an employee', 'Update the role of an employee', 'Update the manager of an employee']
+      choices:['View all departments', 'Add a department', 'Delete a department', 'View all roles', 'Add a role', 'delete a role', 'View all employees', 'Add an employee', 'Update the role of an employee', 'Update the manager of an employee', 'Delete an employee']
     }
   ]).then(res => {
   switch (res.action){
@@ -49,6 +49,12 @@ const promptUser = () => {
       break;
     case 'Delete a department':
       deleteDept()
+      break;
+    case 'Delete a role':
+      deleteRole()
+      break;
+    case 'Delete an employee':
+      deleteEmployee()
       break;
   }
 })
@@ -195,9 +201,12 @@ const updateEmployeeManager = () => {
     promptUser();
   })
 }
-// BONUS delete departments, roles, and employees (DELETE FROM   WHERE)
 //BONUS department delete function
 const deleteDept = () => {
+  db.query("SELECT * FROM department", function(err, result, fields){
+    console.table(result);
+  })
+
   inquirer.prompt([
     {
       type: 'input',
@@ -205,63 +214,47 @@ const deleteDept = () => {
       message: 'What is the department ID?',
     }
   ]).then(res => {
-    const deleteDept = res
-    console.log(deleteDept)
+    console.log(res)
+    const deleteDept = res.id
     db.query("DELETE FROM department WHERE id = ?", deleteDept, function(err, result, fields){
-    console.log(result)
     promptUser()
   })})
 }
 //BONUS role delete function
 const deleteRole = () => {
+  db.query("SELECT * FROM role", function(err, result, fields){
+    console.table(result);
+  })
+
   inquirer.prompt([
     {
       type: 'input',
-      name: 'title',
-      message: 'What is the role title?',
-    },
-    {
-      type: 'input',
-      name: 'salary',
-      message: 'What is the role salary?',
-    },
-    {
-      type: 'input',
-      name: 'department_id',
-      message: 'What department is the role under?',
+      name: 'id',
+      message: 'What is the role ID?',
     }
   ]).then(res => {
-    const newRole = res
-    db.query("INSERT INTO role SET ?", newRole , function(err, result, fields){
+    console.log(res)
+    const deleteRole = res.id
+    db.query("DELETE FROM role WHERE id = ?", deleteRole, function(err, result, fields){
     promptUser()
   })})
 }
 // BONUS employee delete function
 const deleteEmployee = () => {
+  db.query("SELECT * FROM employee", function(err, result, fields){
+    console.table(result);
+  })
+
   inquirer.prompt([
     {
       type: 'input',
-      name: 'first_name',
-      message: 'What is the employee first name?',
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: 'What is the employee last name?',
-    },
-    {
-      type: 'input',
-      name: 'role_id',
-      message: 'What is the employee role id?',
-    },
-    {
-      type: 'input',
-      name: 'manager_id',
-      message: 'What is the employee manager id?',
+      name: 'id',
+      message: 'What is the employee ID?',
     }
   ]).then(res => {
-    const newEmployee = res
-    db.query("INSERT INTO employee SET ?", newEmployee, function(err, result, fields){
+    console.log(res)
+    const deleteEmployee = res.id
+    db.query("DELETE FROM employee WHERE id = ?", deleteEmployee, function(err, result, fields){
     promptUser()
   })})
 }
